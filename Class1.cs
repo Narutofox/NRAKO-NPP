b67d0ff7c1e78d02ceec0ae5402ac2bcae8ea250
Narutofox <icicek@racunarstvo.hr>
2018-08-05 07:39:42 +0200

    Package updates

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NRAKO_IvanCicek
+{
+    public class Class1
+    {
+        //TODO GIt hub - Za svaki od nadolazečih zadataka napraviti u zasebanu granu (branch)
+        //TODO 1. AOP
+        //TODO 2. Unit testovi
+        //TODO 3. Clean code
+
+    }
+}
