8787047a0bd7cbd2664beaaa5a904e50d2d6a55c
Narutofox <icicek@racunarstvo.hr>
2018-08-26 20:41:00 +0200

    UnitTests

---
+++ NPP_UnitTests/HomeControllerUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NRAKO_IvanCicek.Controllers;
+
+namespace NPP_UnitTests
+{
+    [TestClass]
+    public class HomeControllerUnitTest
+    {
+
+        private HomeController _controller;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _controller = new HomeController();
+        }
+        [TestMethod]
+        public void Index()
+        {
+            var result = _controller.Index();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result is ViewResult);
+            Assert.IsTrue(result.ViewName == "");
+            Assert.IsNull(result.Model);
+            Assert.IsNotNull(result.ViewBag.VisibilityOptions);
+            Assert.IsTrue(result.ViewBag.VisibilityOptions == _controller._postsRepo.GetVisibilityOptions());
+            Console.WriteLine();
+        }
+    }
+}
---
+++ NPP_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NPP_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NPP_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6fc68290-6fc4-45d1-982c-948b6a14e5e9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
